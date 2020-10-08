db3eaef35354e53189b63c6de61aa60e86c430db
William Schueller <william.schueller@gmail.com>
2020-10-08 23:40:14 +0200

    added auto load of api key from env var, added token to travis

--- repo_tools/repo_crawler.py
+++ repo_tools/repo_crawler.py
@@ -475,6 +475,9 @@ class RepoCrawler(object):
 		else:
 			api_keys = []
 
+		if len(os.environ['GITHUB_API_KEY']):
+			api_keys.append(os.environ['GH_API_KEY'])
+
 		self.github_requesters = [github.Github(per_page=per_page)]
 		for ak in set(api_keys):
 			g = github.Github(ak,per_page=per_page)
