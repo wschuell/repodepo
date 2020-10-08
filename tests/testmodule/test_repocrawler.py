196b1c691609acc63fc893ba6e791375dd477367
William Schueller <william.schueller@gmail.com>
2020-10-08 23:21:19 +0200

    scaling down number of github queries in tests

--- tests/testmodule/test_repocrawler.py
+++ tests/testmodule/test_repocrawler.py
@@ -49,17 +49,17 @@ def test_clone(testrc):
 	testrc.clone_all()
 
 def test_clone_inexistant(testrc):
-	testrc.add_list(['wschuell/experiment_manager','flowersteam/naminggamesal'],source='GitHub',source_urlroot='github.com')
+	testrc.add_list(['wschuell/experiment_manager'],source='GitHub',source_urlroot='github.com')
 	testrc.add_list(['wschuell/inexistantrepo'],source='GitHub',source_urlroot='github.com')
 	testrc.clone_all()
 
 def test_gitfetch(testrc):
-	testrc.add_list(['wschuell/experiment_manager','flowersteam/naminggamesal'],source='GitHub',source_urlroot='github.com')
+	testrc.add_list(['wschuell/experiment_manager'],source='GitHub',source_urlroot='github.com')
 	testrc.clone_all()
 	testrc.clone_all(update=True)
 
 def test_fillcommits(testrc):
-	testrc.add_list(['wschuell/experiment_manager','flowersteam/naminggamesal'],source='GitHub',source_urlroot='github.com')
+	testrc.add_list(['wschuell/experiment_manager'],source='GitHub',source_urlroot='github.com')
 	testrc.clone_all()
 	testrc.fill_commit_info()
 
