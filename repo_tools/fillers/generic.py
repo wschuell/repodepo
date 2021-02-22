3c9a386282353ee556da50b6c3b2c11ea9df1375
William Schueller <william.schueller@gmail.com>
2021-02-22 19:38:42 +0100

    debugging travis issue with pygit2.clone

--- repo_tools/fillers/generic.py
+++ repo_tools/fillers/generic.py
@@ -479,11 +479,16 @@ class ClonesFiller(fillers.Filler):
 			# if (self.db.cursor.fetchone() is None) or force:
 			self.logger.info('Cloning repo {}/{}/{}'.format(source,owner,name))
 			try:
-				self.logger.info(subprocess.check_output('ls -al'))
-				self.logger.info(subprocess.check_output('ls -al dummy_clones'))
-				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos'))
-				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos/GitHub'))
-				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos/GitHub/*'))
+				self.logger.info('ls -al')
+				self.logger.info(subprocess.check_output('ls -al'.split(' ')))
+				self.logger.info('ls -al dummy_clones')
+				self.logger.info(subprocess.check_output('ls -al dummy_clones'.split(' ')))
+				self.logger.info('ls -al dummy_clones/cloned_repos')
+				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos'.split(' ')))
+				self.logger.info('ls -al dummy_clones/cloned_repos/GitHub')
+				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos/GitHub'.split(' ')))
+				self.logger.info('ls -al dummy_clones/cloned_repos/GitHub/*')
+				self.logger.info(subprocess.check_output('ls -al dummy_clones/cloned_repos/GitHub/*'.split(' ')))
 			except FileNotFoundError as e:
 				self.logger.info(e)
 			try:
