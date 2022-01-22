fffd941d04d237dda776df7a6ed9b45fe5de1b35
William Schueller <william.schueller@gmail.com>
2022-01-22 10:59:09 +0100

    debugging tests on github actions

--- tests/testmodule/test_fillers_gitlab.py
+++ tests/testmodule/test_fillers_gitlab.py
@@ -21,7 +21,9 @@ def testdb(request):
 	db = repo_tools.repo_database.Database(db_name='travis_ci_test_repo_tools',db_type=request.param,data_folder='dummy_clones')
 	db.clean_db()
 	db.init_db()
-	return db
+	yield db
+	db.connection.close()
+	del db
 
 workers = 1 #5
 
@@ -35,7 +37,7 @@ def test_gitlab(testdb):
 	testdb.add_filler(generic.RepositoriesFiller())
 	testdb.add_filler(generic.ClonesFiller(data_folder='dummy_clones')) # Clones after forks to have up-to-date repo URLS (detect redirects)
 	testdb.add_filler(commit_info.CommitsFiller(data_folder='dummy_clones')) # Commits after forks because fork info needed for repo commit ownership
-	# testdb.add_filler(gitlab_gql.LoginsFiller(fail_on_wait=True,workers=workers))
+	testdb.add_filler(gitlab_gql.LoginsFiller(fail_on_wait=True,workers=workers))
 	testdb.fill_db()
 
 # Deactivated LoginsFiller: Causing trouble on Github Actions. Missing properties of the Requester class (remaining query count, time to reset)
