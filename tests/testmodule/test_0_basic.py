ade0ab52b0e475a5c6e12c20c8c8d0963764b064
William Schueller <william.schueller@gmail.com>
2022-05-04 05:25:03 +0200

    name parsed from setup.py instead of folder name

--- tests/testmodule/test_0_basic.py
+++ tests/testmodule/test_0_basic.py
@@ -7,6 +7,8 @@ def test_basic():
 def test_import():
 	path = os.path.abspath(__file__)
 	dir_name = os.path.dirname(os.path.dirname(os.path.dirname(path)))
-	libname = os.path.basename(dir_name)
+	with open(os.path.join(dir_name,'setup.py'),'r') as f:
+		file_content = f.read()
+	libname = file_content.split('setup(')[1].split("name='")[1].split("'")[0]
 	importlib.import_module(libname)
 
